29ee2ac8aee743f280b8cad12856ddaa7ad58e77
AnzinFelipe <ian.felipe.drunen@gmail.com>
2025-09-23 16:50:48 -0300

    Atualizando nomes do projeto e app

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wondehoy.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wondercooking.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- postagens/apps.py
+++ postagens/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WonderimagemConfig(AppConfig):
+class PostagensConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'wonderimagem'
+    name = 'postagens'
